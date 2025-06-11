97011aa61d83979988b5d4ddebd07673186a8057
ujnss <unlinunlin@163.com>
2025-06-11 09:50:27 +0000

    update

--- zktls/app/src/main.rs
+++ zktls/app/src/main.rs
@@ -8,17 +8,15 @@ pub fn main() {
     let attestation_data: String = read_as();
     println!("attestation_data {:?}", attestation_data);
 
-    // let attestation_data: AttestationData = serde_json::from_str(&attestation_data).unwrap();
-    // let messages = attestation_data.verify().unwrap();
+    let attestation_data: AttestationData = serde_json::from_str(&attestation_data).unwrap();
+    let (messages, records) = attestation_data.verify().unwrap();
 
-    // let mut json_paths = vec![];
-    // json_paths.push("$.data.spotVol");
-    // json_paths.push("$.data.spotNeed");
-    // let json_value = messages[0].get_json_values(&json_paths);
-    // println!("json value:{:?}", json_value);
+    let mut json_paths = vec![];
+    json_paths.push("$.data.spotVol");
+    json_paths.push("$.data.spotNeed");
+    let json_value = messages[0].get_json_values(&json_paths);
+    println!("json value:{:?}", json_value);
+    println!("records: {}", records);
 
-    // commit(&verifying_key);
-    // commit(&verifying_data.get_records());
-    let p = vec![0x31u8];
-    commit(&p);
+    commit(&records);
 }
