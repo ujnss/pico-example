f8204b5663d605eb95371b3bc8a2911a5d2818ec
ujnss <unlinunlin@163.com>
2025-06-12 09:30:17 +0000

    update

--- start_server.sh
+++ start_server.sh
@@ -4,4 +4,10 @@ curdir=$(pwd)
 
 # zktls
 cd ${curdir}/server
+
+export JEMALLOC_SYS_WITH_MALLOC_CONF="retain:true,background_thread:true,metadata_thp:always,dirty_decay_ms:-1,muzzy_decay_ms:-1,abort_conf:true"
+export CHUNK_SIZE=4194304
+export CHUNK_BATCH_SIZE=32
+export SPLIT_THRESHOLD=1048576
+export RUST_LOG=info
 python3 https_server.py
